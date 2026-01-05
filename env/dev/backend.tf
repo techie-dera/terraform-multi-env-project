8564bcd77afb9928fe6aa2f87faabb51f52f7c69
techie-dera <deraenyelu@gmail.com>
2026-01-05 20:02:52 +0400

    fix: dev outputs for web app

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-acmepay-tfstate"
-    storage_account_name = "acmepaytfst4125mzgaga"
+    storage_account_name = "acmepaytfst4125k3jwin"
     container_name       = "tfstate"
     key                  = "acmepay-dev.tfstate"
   }

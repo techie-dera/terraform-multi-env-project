43c55cee1e415cd6f892b8300491521ca09db9d5
techie-dera <deraenyelu@gmail.com>
2026-01-06 15:08:03 +0400

    updated fix

--- env/prod/backend.tf
+++ env/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-acmepay-tfstate"
-    storage_account_name = "REPLACE_WITH_BOOTSTRAP_OUTPUT"
+    storage_account_name = "acmepaytfst4125k3jwin"
     container_name       = "tfstate"
     key                  = "acmepay-prod.tfstate"
   }
